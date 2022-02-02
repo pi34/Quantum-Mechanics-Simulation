dbe2299d8e9ae141c8802889e4d88a249e1d4f33
Riya Manchanda <59563727+pi34@users.noreply.github.com>
2022-02-02 19:38:44 +0530

    Update quantumMechanicsSimulation.py

--- quantumMechanicsSimulation.py
+++ quantumMechanicsSimulation.py
@@ -1,12 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from matplotlib.widgets import Slider, Button
 
 L = float(input())
-N = int(input())
+N = 2
 
-def wave (x):
-    return (np.sqrt(2/L)) * np.sin(((N * np.pi) / L) * x)
+plt.style.use('dark_background')
+
+fig = plt.figure()
+ax = fig.subplots()
+
+def wave (x, l = L, N = 2):
+    return (np.sqrt(2/l)) * np.sin(((N * np.pi) / l) * x)
 
 def probDist (wave):
     return wave ** 2
@@ -24,25 +30,53 @@ proby = probDist(y)
 for i in range (int(L)+1):
     x1.append(i)
     y1.append(0)
+    
+def gen(N):
 
-area = []
+    area = []
 
-curr = 0
+    curr = 0
 
-while curr <= L:
-    area.append(int((probDist(wave(curr)) * 0.01) * 10000))
-    curr = curr + 0.01
+    while curr <= L:
+        area.append(int((probDist(wave(curr, N)) * 0.01) * 10000))
+        curr = curr + 0.01
 
-probailities = []
+    probailities = []
 
-for i in range(len(area)):
-    for j in range(area[i]+1):
-        probailities.append(0.01 * j)
+    for i in range(len(area)):
+        for j in range(area[i]+1):
+            probailities.append(0.01 * j)
 
-point = random.choice(probailities)
+    point = random.choice(probailities)
+    ax.plot(point, 0, 'go')
 
-plt.plot(x, y, color="red")
-plt.plot(x, proby, color="blue")
-plt.plot(x1, y1)
-plt.plot(point, 0, "go)
+plot, = ax.plot(x, y, color="red")
+plot2, = ax.plot(x, proby, color="blue")
+ax.plot(x1, y1)
+gen(N)
+
+plt.subplots_adjust(bottom=0.25)
+
+ax3 = (plt.axes([0.25, 0.12, 0.65, 0.03]))
+ax4 = (plt.axes([0.8, 0.05, 0.1, 0.05]))
+
+allowed_amplitudes = np.concatenate([np.linspace(1, 1, 100), range(1, 101)])
+
+sN = Slider(ax3, "Energy Level", 1, 100, valinit=N, valstep=allowed_amplitudes)
+
+button = Button(ax4, 'Plot', color='darkgoldenrod', hovercolor='indigo')
+
+def func(event):
+    gen(sN.val)
+
+button.on_clicked(func)
+
+def update(val):
+    y = wave(x, L, sN.val)
+    proby = probDist(y)
+    plot.set_ydata(y)
+    plot2.set_ydata(proby)
+
+sN.on_changed(update)
+
 plt.show()
